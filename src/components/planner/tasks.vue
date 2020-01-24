5fed027d1aa8d0ceaeeb7c5767058c9841174efd
damien verkerk <damien-verkerk@hotmail.com>
2020-01-24 13:26:05 +0100

    Task on complete delete

--- src/components/planner/tasks.vue
+++ src/components/planner/tasks.vue
@@ -1,7 +1,14 @@
 <template>
   <div  v-if="tasksArray">
     <ul class="list-inside sm:list-outside md:list-inside lg:list-outside xl:list-inside">
-    <li v-for="task in getTasksByProjectId(projectId)" >{{ task.name}}</li>
+    <li v-for="task in getTasksByProjectId(projectId)" >
+      <input 
+      class="mr-2 leading-tight" 
+      type="checkbox" v-model="checked" 
+      @change="onCheck(task.id)"
+      :value="task.name"
+      >
+      {{ task.name}}</li>
     </ul>
   </div>
 </template>
@@ -13,11 +20,21 @@ export default {
   name: "Tasks",
   data: function() {
     return {
+      checked: false,
         tasksArray : [],
         tasksQuery : false
     };
   },
   methods: {
+    onCheck: function(completedTask) {
+      console.log(this.checked , completedTask)
+      fb.tasksCollection.doc(completedTask).delete().then(function(){
+        console.log(completedTask + " is deleted")
+      }).catch(function(error){
+        console.log("Error removing document: " , error)
+      })
+      
+    }
   },
   computed: {
      getTasksByProjectId () {
